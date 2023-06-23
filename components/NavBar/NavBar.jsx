5b517e7c6bf07c2a1d7a7c8d2dfd35b1bdf501b0
Harsh Sinha <85688366+harsh1sinha@users.noreply.github.com>
2023-06-23 09:49:44 +0530

    navbar done

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from "next/image";
+import Style from './NavBar.module.css';
+import image from "../../img";
+import { Button } from '../componentsindex';
+import Link from 'next/link';
+
+
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={Style.navbar}>
+      <div className={Style.rect1}>
+      
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo_img}>
+         <Image 
+         className={Style.logo_img}
+          src = {image.catlogo}
+          alt = "Cat logo"
+          />
+          </div>
+          <div className={Style.Github}>Github</div>
+          <div className={Style.exp}> EXplore</div>
+         
+        </div>
+        {/* <div className={Style.login}>Login</div> */}
+        <button className={Style.login}>Sign-in</button>
+        </div>
+    </div>
   )
 }
 
